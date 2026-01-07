8bb8838654c43e0ec146324c6716828ea7b1b086
drakelei1140-coder <drake.lei.1140@gmail.com>
2026-01-07 22:51:52 +0800

    Squashed commit of the following:
    commit 106c91a2591768c2fb98c1dd4d029d12277b4346
    Author: drakelei1140-coder <drake.lei.1140@gmail.com>
    Date:   Wed Jan 7 22:41:41 2026 +0800
    
        1
    
    commit 50bee2bdad8025f4c030ce245f48706e5506edf3
    Merge: da62ff9 f4f9822
    Author: drakelei1140-coder <drake.lei.1140@gmail.com>
    Date:   Wed Jan 7 22:21:07 2026 +0800
    
        Merge branch 'codex/add-rotation-attack-logic-to-enemy_01' of https://github.com/drakelei1140-coder/slimefrontier into codex/add-rotation-attack-logic-to-enemy_01
    
    commit da62ff9edbb5dcf42a9befea01adefca7e8b6c0a
    Author: drakelei1140-coder <drake.lei.1140@gmail.com>
    Date:   Wed Jan 7 22:20:49 2026 +0800
    
        Update enemy_01.tscn
    
    commit f4f982207b7aa918d53ad63d32e57629ab5c793f
    Author: drakelei1140-coder <drake.lei.1140@gmail.com>
    Date:   Wed Jan 7 22:15:41 2026 +0800
    
        Add one-shot damage dealer for Enemy_01 attack
    
    commit ab983e86f50ba504f4196078cc5992b92d1baae3
    Author: drakelei1140-coder <drake.lei.1140@gmail.com>
    Date:   Wed Jan 7 22:15:30 2026 +0800
    
        Center Enemy_01 attack hitbox for rotation
    
    commit a52cf5d85267842f3eb75f93c0b9a236e0b1c0e2
    Author: drakelei1140-coder <drake.lei.1140@gmail.com>
    Date:   Mon Jan 5 17:47:52 2026 +0800
    
        Add rotating attack for Enemy_01

--- main/Enemy_01.gd
+++ main/Enemy_01.gd
@@ -4,12 +4,27 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
-@onready var enemy_attack_hitbox: Area2D = $AttackHitbox
+@export var attack_cd: float = 3.0
+@export var swing_duration: float = 1.0
+@export var start_angle_offset: float = -PI / 3.0
+@export var hitbox_radius: float = 40.0
+@export var attack_damage: int = 2
+@export var attack_stagger: float = 0.15
+@export var attack_target_group: StringName = &"player"
+
+var _attack_cd_left: float = 0.0
+var _swing_active: bool = false
+var _swing_elapsed: float = 0.0
+var _swing_start_angle: float = 0.0
+
+@onready var attack_hitbox:OneShotDamageDealer = $AttackHitbox
 
 func _ready() -> void:
-	enemy_attack_hitbox.monitoring = false
-	enemy_attack_hitbox.monitorable = false
-	enemy_set_attack_hitbox_active(false)
+	attack_hitbox.set_active(false)
+	attack_hitbox.damage_amount = attack_damage
+	attack_hitbox.stagger_amount = attack_stagger
+	attack_hitbox.target_group = attack_target_group
+
 	var role_hurtbox: HurtboxEnemy = $Hurtbox as HurtboxEnemy
 	if is_instance_valid(role_hurtbox):
 		role_hurtbox.hurtbox_hit.connect(_on_role_hurtbox_hit)
@@ -22,6 +37,8 @@ func _on_role_hurtbox_hit(damage: int, stagger: float, _source: Node) -> void:
 	print("Enemy hit! damage=", damage, " stagger=", stagger)
 
 func _physics_process(delta: float) -> void:
+	_update_attack(delta)
+
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -39,8 +56,55 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
-	
-func enemy_set_attack_hitbox_active(is_active: bool) -> void:
 
-	enemy_attack_hitbox.monitoring = is_active
-	enemy_attack_hitbox.monitorable = is_active
+func _update_attack(delta: float) -> void:
+	if _swing_active:
+		_swing_elapsed += delta
+		_update_attack_hitbox_position()
+
+		if _swing_elapsed >= swing_duration:
+			_end_swing()
+		return
+
+	_attack_cd_left = maxf(_attack_cd_left - delta, 0.0)
+	if _attack_cd_left <= 0.0:
+		_start_swing()
+
+
+func _start_swing() -> void:
+	_swing_active = true
+	_swing_elapsed = 0.0
+
+	var dir_to_player := _get_direction_to_target()
+	_swing_start_angle = dir_to_player.angle() + start_angle_offset
+
+	attack_hitbox.begin_attack()
+	attack_hitbox.set_active(true)
+	_update_attack_hitbox_position()
+
+
+func _end_swing() -> void:
+	_swing_active = false
+	_swing_elapsed = 0.0
+	_attack_cd_left = attack_cd
+	attack_hitbox.set_active(false)
+	attack_hitbox.position = Vector2.ZERO
+
+
+func _update_attack_hitbox_position() -> void:
+	var duration := maxf(swing_duration, 0.001)
+	var angle := _swing_start_angle + (_swing_elapsed / duration) * TAU
+	attack_hitbox.position = Vector2(cos(angle), sin(angle)) * hitbox_radius
+
+
+func _get_direction_to_target() -> Vector2:
+	var targets := get_tree().get_nodes_in_group(attack_target_group)
+	for target in targets:
+		var node2d := target as Node2D
+		if node2d == null:
+			continue
+
+		var to_target := node2d.global_position - global_position
+		if to_target.length() > 0.001:
+			return to_target.normalized()
+	return Vector2.RIGHT
